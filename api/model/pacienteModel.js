42d97d8018ac95471bc43bf00f6540fdcc2e56d3
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-10-14 17:53:19 -0500

    feat: :sparkles: mejora al modelo de paciente al obtener la informacion

--- api/model/pacienteModel.js
+++ api/model/pacienteModel.js
@@ -7,13 +7,31 @@ class Paciente extends Conexion{
         try {
             let driver = await this.conexion;
             const [results] = await driver.data.query(
-                'select * from paciente'
+                `SELECT 
+                    d.cedula AS Cedula,
+                    d.nombre AS Nombre,
+                    d.apellido AS Apellido,
+                    d.genero AS Genero,
+                    d.fecha_nacimiento AS Fecha_nacimiento,
+                    d.edad AS Edad,
+                    d.estado AS Estado,
+                    GROUP_CONCAT(CASE WHEN cd.tipo = 'Telefono' THEN cd.contacto END) AS Telefono,
+                    GROUP_CONCAT(CASE WHEN cd.tipo = 'Celular' THEN cd.contacto END) AS Celular,
+                    GROUP_CONCAT(CASE WHEN cd.tipo = 'Correo electronico' THEN cd.contacto END) AS Correo_Electronico
+                FROM 
+                    paciente d
+                LEFT JOIN 
+                    comunicacion_paciente cd ON d.Cedula = cd.paciente_fk
+                GROUP BY 
+                    d.Cedula, d.Nombre, d.Apellido, d.Genero, d.Fecha_nacimiento, d.Edad, d.Estado
+                ORDER BY 
+                    d.Cedula;`
             );
-            return {status: 200, message: "Lista de Pacientes", data: results};
+            return { status: 200, message: "Lista de Pacientes", data: results };
         } catch (error) {
-            throw new Error(JSON.stringify({ status: 500, message: "Ocurrio un error al obtener todos los pacientes", data: error}))
+            throw new Error(JSON.stringify({ status: 500, message: "Ocurrió un error al obtener todos los pacientes", data: error }));
         }
-    }
+    }    
     async informacionPaciente(cedula) {
         try {
             let driver = await this.conexion;
