8de5385f470ce0ccf1aee0fd358e51642d75e858
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-10-18 08:49:03 -0500

    feat: :sparkles: uso de express limit en hospitales

--- api/version/hospitalV1.js
+++ api/version/hospitalV1.js
@@ -1,10 +1,11 @@
 // version
 const express = require('express');
-const hospitalController = require('../controller/hospitalController')
+const hospitalController = require('../controller/hospitalController');
+const limite = require('../limit/hospitalLimit');
 const hospital = express();
 
 hospital.get("/", hospitalController.showHospital);
-hospital.get("/todos", hospitalController.obtenerTodosLosHospitales);
+hospital.get("/todos", limite.obtenerTodosLosHospitales, hospitalController.obtenerTodosLosHospitales);
 hospital.get("/:id", hospitalController.obtenerUnHospitalPorId);
 hospital.post("/", express.json(), hospitalController.guardarHospital);
 hospital.delete("/:id", hospitalController.eliminarHospital);
