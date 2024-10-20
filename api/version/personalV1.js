e989bc7f6222508860ffd3626e894e5532253473
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-10-20 12:14:57 -0500

    feat: :sparkles: uso de express limit en personal

--- api/version/personalV1.js
+++ api/version/personalV1.js
@@ -1,9 +1,10 @@
 // version
 const express = require('express');
 const personalController = require('../controller/personal.controller');
+const limite = require('../limit/personalLimit');
 const personal = express();
 
-personal.get("/", personalController.obtenerPersonal);
+personal.get("/", limite.obtenerTodosLosPersonal,  personalController.obtenerPersonal);
 
 
 module.exports = personal;
